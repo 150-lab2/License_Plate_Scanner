a22e19444c51e8a3b01daf9aee921425213ac92a
e-pimentel <emanuelpimentel672@gmail.com>
2023-10-09 15:09:01 -0700

    Added Log In POST Request

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,10 @@ let userSchema = mongoose.Schema({
 
 
 // Log requests to console
-app.use('/', function(req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-  });
+app.use('/', (req, res, next) => {
+        console.log(req.method + " " + req.path + " - " + req.ip);
+        next();
+    });
 
 // Set up body-paser
 app.use(bodyParser.json())
@@ -51,26 +51,31 @@ app.use('/css', express.static(path.join(__dirname,'..','frontend','css')));
 app.use('/images', express.static(path.join(__dirname,'..','frontend','images')));
 
 // GET requests
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,'..','frontend','index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
-app.get('/contact', function(req, res) {
-    res.sendFile(path.join(__dirname,'..','frontend','index.html'))
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
-app.get('/log_in', function (req, res) {
-    res.sendFile(path.join(__dirname,'..','frontend','log_in.html'));
+app.get('/log_in', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'log_in.html'));
 });
-app.get('/sign_up', function (req, res) {
-    res.sendFile(path.join(__dirname,'..','frontend','sign_up.html'));
+app.get('/sign_up', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'sign_up.html'));
 });
 
 // POST requests
-app.post('/sign_up', function(req, res) {
+app.post('/log_in', (req, res) => {
+    console.log(req.body);
+    res.redirect('log_in');
+});
+
+app.post('/sign_up', (req, res) => {
     console.log(req.body);
     res.redirect('/sign_up');
 });
 
 // Host locally on port 5000
-app.listen(5000, function () {
+app.listen(5000, () => {
     console.log('Running on port 5000');
 });
